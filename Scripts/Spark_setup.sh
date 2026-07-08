b87c3f4534e13b5ffb4c78e052a2f083e0beb4d9
James Radtke <cloudxabide@gmail.com>
2026-07-08 09:11:42 -0400

    Starting Over

--- Scripts/Spark_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# You should start open-webui:ollama - using "../bin/ollamactl start"
-
-# Pull the Nemotron-3 model
-docker exec -it $(docker ps -a | grep ollama | awk '{ print $1 }') "ollama pull nemotron-3-super:120b"
-# OR (this needs to be tested)
-docker exec -it $(docker ps -q -f "name=ollama" | head -n 1) ollama pull nemotron-3-super:120b
